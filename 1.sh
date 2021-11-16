af0feca9f9f5eda0eaa5dfb51bacb560740723f4
Andrei Kvapil <kvapss@gmail.com>
2021-11-16 12:29:05 +0100

    add parallelism

--- 1.sh
+++ 1.sh
@@ -1,47 +1,28 @@
 #!/bin/bash
 IP=$1
 PATTERN=$2
+JOBS=${JOBS:-3}
 
 if [ -z "$PATTERN" ]; then
   "Usage: $0 <3par> <pattern>"
   exit 1
 fi
 
-# Setup host
-INITIATOR_NAME=$(awk -F= '{print $2}' /etc/iscsi/initiatorname.iscsi)
-HOST=$(hostname -s)
-
-if [ -z "$INITIATOR_NAME" ] || [ -z "$HOST" ] || [ -z "$IP" ]; then
-  echo "INITIATOR_NAME, HOST or IP is empty!" >&2
-  exit 1
-fi
-
-ssh "3paradm@$IP" createhost -iscsi "$HOST" "$INITIATOR_NAME"
-
-# ---------------------------
-
-RPORTALS=$(ssh "3paradm@$IP" showport -iscsivlans | tail -n+2 | head -n-2 | awk '{print $3}')
-LPORTALS=$(sudo iscsiadm -m session -o show | awk -F '[ :,]+' '{print $3}')
-
-# iSCSI login
-for RPORTAL in $RPORTALS; do
-  for LPORTAL in $LPORTALS; do
-    if [ "$LPORTALS" = "$RPORTAL" ]; then
-      continue 2
-    fi
+exec_async() {
+  # content of script exec-async.sh
+  joblist=($(jobs -p))
+  while (( ${#joblist[*]} >= $JOBS )); do
+      sleep 1
+      joblist=($(jobs -p))
   done
-  sudo iscsiadm -m discovery -t sendtargets -p "$RPORTAL"
-  sudo iscsiadm -m node -l all -p "$RPORTAL"
-done
+  echo "Starting job: $*"
+  $* &
+}
 
-VVS=$(ssh "3paradm@$IP" showvv -showcols Name "$PATTERN" | head -n-2 | tail -n+2 | grep -v '\-backup ')
-
-# Perform backup
-for VV in $VVS; do
-  (
-    LUN=99
+backup_vv() {
+  LUN=$1 VV=$2 HOST=$HOST IP=$IP bash -sx <<\EOT
     # cleanup broken devices (just in case)
-    OLD_VVS=$(ssh "3paradm@$IP" showvlun -host "$HOST" | awk "\$1 == $LUN && \$NF == \"host\" {print \$2}" )
+    OLD_VVS=$(ssh "3paradm@$IP" showvlun -host "$HOST" </dev/null | awk "\$1 == $LUN && \$NF == \"host\" {print \$2}" )
     for OLD_VV in $OLD_VVS; do
       yes | ssh "3paradm@$IP" removevlun "$OLD_VV" "$LUN" "$HOST"
       iscsiadm -m session --rescan "$RPORTALS"
@@ -60,7 +41,7 @@ for VV in $VVS; do
     # attach
     yes | ssh "3paradm@$IP" createsv -ro -exp 7d "$VV-backup" "$VV"
     yes | ssh "3paradm@$IP" createvlun "$VV-backup" "$LUN" "$HOST"
-    WWN=$(ssh "3paradm@$IP" showvv -showcols VV_WWN "$VV-backup" | awk 'FNR == 2 {print tolower($1)}')
+    WWN=$(ssh "3paradm@$IP" showvv -showcols VV_WWN "$VV-backup" </dev/null | awk 'FNR == 2 {print tolower($1)}')
     iscsiadm -m session --rescan "$RPORTALS"
     ISCSI_DISKS=$(iscsiadm -m session -P3 | grep "Lun: $LUN$" -A1 | awk '/Attached scsi disk/ {print $4}')
     multipath $ISCSI_DISKS
@@ -90,8 +71,50 @@ for VV in $VVS; do
 
     yes | ssh "3paradm@$IP" removevlun "$VV-backup" "$LUN" "$HOST"
     yes | ssh "3paradm@$IP" removevv "$VV-backup"
-  )
+EOT
+}
+
+# Setup host
+INITIATOR_NAME=$(awk -F= '{print $2}' /etc/iscsi/initiatorname.iscsi)
+HOST=$(hostname -s)
+
+if [ -z "$INITIATOR_NAME" ] || [ -z "$HOST" ] || [ -z "$IP" ]; then
+  echo "INITIATOR_NAME, HOST or IP is empty!" >&2
+  exit 1
+fi
+
+ssh "3paradm@$IP" createhost -iscsi "$HOST" "$INITIATOR_NAME"
+
+# ---------------------------
+
+RPORTALS=$(ssh "3paradm@$IP" showport -iscsivlans | tail -n+2 | head -n-2 | awk '{print $3}')
+LPORTALS=$(sudo iscsiadm -m session -o show | awk -F '[ :,]+' '{print $3}')
+
+# iSCSI login
+for RPORTAL in $RPORTALS; do
+  for LPORTAL in $LPORTALS; do
+    if [ "$LPORTALS" = "$RPORTAL" ]; then
+      continue 2
+    fi
+  done
+  sudo iscsiadm -m discovery -t sendtargets -p "$RPORTAL"
+  sudo iscsiadm -m node -l all -p "$RPORTAL"
 done
+
+VVS=$(ssh "3paradm@$IP" showvv -showcols Name "$PATTERN" | head -n-2 | tail -n+2 | grep -v '\-backup ')
+VVS_WC=$(echo "$VVS" | wc -l)
+PORTION="$(($VVS_WC/$JOBS))"
+FIRST_LUN=100
+
+# Set traps to kill background processes
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+## Perform backup
+while read LUN VV; do
+  exec_async backup_vv "$LUN" "$VV"
+done < <(echo "$VVS" | awk -v FIRST_LUN="$FIRST_LUN" -v COUNT="$JOBS" '{ f=(f%COUNT)+1 ; print FIRST_LUN + f-1, $0 }')
+
 
 iscsiadm -m session --rescan $RPORTALS
 
